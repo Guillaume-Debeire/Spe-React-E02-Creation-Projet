fa5fea59c02e7d66e2648c8317f0586a4091b2fd
guillaume-debeire <guillaume@debeire.com>
2021-06-10 12:41:38 +0200

    react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const title = <h1>Bonjour</h1>;
 
+console.log(title);
 
-const titleVanilla = document.createElement('h1');
-titleVanilla.textContent = "bonjour";
+ReactDOM.render(title, document.getElementById('root'));
